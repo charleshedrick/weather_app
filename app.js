d8615de4240f9fd9225caf4e0c000802cf09753a
Charles Hedrick <hedrickcharles@gmail.com>
2023-08-12 19:47:57 -0400

    Basic API done without icons.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // SELECT ELEMENTS
-const iconElement = document.querySelector(".weather-icon");
+//const iconElement = document.querySelector(".weather-icon"); //need to find a way to select the icons with the NWS data (no easy icon values)
 const tempElement = document.querySelector(".temp-value p");
 const descElement = document.querySelector(".temp-description p");
 const locationElement = document.querySelector(".location p");
@@ -27,7 +27,7 @@ if ("geolocation" in navigator) {
 
 // SET USER'S POSITION
 function setPosition(position) {
-  let latitude = position.coords.latutude;
+  let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
 
   getWeather(latitude, longitude);
@@ -39,13 +39,54 @@ function showError(error) {
   notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
-/*
+// GET WEATHER FROM WEATHER.GOV API
 function getWeather(latitude, longitude) {
-  let api = `https://api.weather.gov/points/{lat=${latitude}},{lon=${longitude}}`;
+  const apiUrl = `https://api.weather.gov/points/${latitude},${longitude}`;
+
+  // Make the GET request using Fetch API
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    // lat/long data from site is used to fetch local forecast data
+    .then((locationData) => {
+      const forecastUrl = locationData.properties.forecast;
+      console.log(locationData);
+      weather.city = locationData.properties.relativeLocation.properties.city;
+      weather.state = locationData.properties.relativeLocation.properties.state;
+      console.log(locationData.properties.relativeLocation.properties.state);
+      return fetch(forecastUrl); //fetches the current locations weather data from the API and formats it to a fetchable api url.
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((forecastData) => {
+      // Handle and display the forecast data
+      console.log(forecastData);
+      // Extract and display the forecast details as needed
+      weather.temperature.value =
+        forecastData.properties.periods[0].temperature;
+      weather.description = forecastData.properties.periods[0].shortForecast;
+      console.log(forecastData.properties.periods[0].shortForecast);
+      //weather.iconId = data.weather[0].icon;
+    })
+    .then(() => {
+      displayWeather();
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+    });
 }
 
-fetch(api) .then(function(response)) {
-    let data = response.json();
-    return data;
-};
-*/
+// DISPLAY WEATHER TO UI
+function displayWeather() {
+  tempElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;
+  descElement.innerHTML = weather.description;
+  locationElement.innerHTML = `${weather.city}, ${weather.state}`;
+}
